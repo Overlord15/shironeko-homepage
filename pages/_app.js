353b24f205a27cc2cb47d8492f893e1319c65678
Anupam <khamrai.eren.3@gmail.com>
2022-06-12 13:45:46 +0530

    fixed dark theme

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,35 @@
+import React from 'react'
 import { ChakraProvider } from '@chakra-ui/react'
-// import theme from '../pages/home/theme'
 import Font from '../pages/home/font'
+import { extendTheme } from '@chakra-ui/react'
+import { mode } from '@chakra-ui/theme-tools'
+
+const styles = {
+  global: props => ({
+    body: {
+      bg: mode('#f0e7db', '#202023')(props)
+    }
+  })
+}
+
+
+const config = {
+  initialColorMode: 'dark',
+  useSystemColorMode: true
+}
+
+const theme = extendTheme({ config, styles })
 
 function MyApp({ Component, pageProps }) {
+
+
   return (
-    <ChakraProvider>
-      <Font />
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <React.StrictMode>
+      <ChakraProvider theme={theme}>
+        <Font />
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </React.StrictMode>
   )
 }
 
--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,27 @@
 import { ColorModeScript } from '@chakra-ui/react'
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
-// import theme from '../pages/home/theme'
+
+const config = {
+    initialColorMode: 'dark',
+    useSystemColorMode: true
+}
+
+const styles = {
+    global: props => ({
+        body: {
+            bg: mode('#f0e7db', '#202023')(props)
+        }
+    })
+}
 
 export default class Document extends NextDocument {
+
     render() {
         return (
             <Html lang="en">
-                <Head>
-                </Head>
+                <Head />
                 <body>
-                    {/* <ColorModeScript initialColorMode={theme.config.initialColorMode} /> */}
+                    <ColorModeScript initialColorMode={config.initialColorMode} />
                     <Main />
                     <NextScript />
                 </body>
